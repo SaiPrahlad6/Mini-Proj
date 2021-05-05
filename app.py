f0a8de0ecf63761493a9667621499792010d00ca
SaiPrahlad6 <krishnaprahladm@gmail.com>
2021-05-05 19:28:04 +0530

    Changes

--- app.py
+++ app.py
@@ -15,42 +15,47 @@ model = pickle.load(open('chronic_k_d.pkl', 'rb'))
 #print(a.shape)
 #pred=(model.predict([[a]]))
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET','POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict([final_features])
-    output=""
-    if(prediction==0):
-        output=("Not a Chronic")
-    else:
-        output=("Chronic")
+    if request.method=='POST':
+        
+        int_features = [float(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = model.predict([final_features])
+        output=""
+        if(prediction==0):
+            output=("Not a Chronic")
+        else:
+            output=("Chronic")
 
     #output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='It is {} a Kidney Disease Presence'.format(output))
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output1=""
-    if(prediction==0):
-        output1=("Not a Chronic")
-    else:
-        output1=("Chronic")
-    return jsonify(output1)
+        return render_template('index.html', prediction_text='It is {} a Kidney Disease Presence'.format(output))
+    return None
+# =============================================================================
+# 
+# @app.route('/predict_api',methods=['POST'])
+# def predict_api():
+#     '''
+#     For direct API calls trought request
+#     '''
+#     data = request.get_json(force=True)
+#     prediction = model.predict([np.array(list(data.values()))])
+# 
+#     output1=""
+#     if(prediction==0):
+#         output1=("Not a Chronic")
+#     else:
+#         output1=("Chronic")
+#     return jsonify(output1)
+# =============================================================================
 
 if __name__ == "__main__":
     app.run(debug=True)
